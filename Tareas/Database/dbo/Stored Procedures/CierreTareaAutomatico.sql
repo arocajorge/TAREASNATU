7e5f48da9f63d085c540e45062d3ba00cb2c5389
carloscede <carloscede@hotmail.es>
2018-10-14 15:08:45 -0500

    ..

---
+++ Tareas/Database/dbo/Stored Procedures/CierreTareaAutomatico.sql
@@ -0,0 +1,37 @@
+﻿CREATE PROCEDURE CierreTareaAutomatico
+AS
+DECLARE @IdTarea numeric
+DECLARE @SecuencialHistorico int
+DECLARE @EsRecursivo bit
+
+SELECT top 1 @IdTarea = IdTarea  FROM Tarea WHERE FechaCierreEncargado >= dateadd(day,3,FechaCierreEncargado) and FechaCierreSolicitante is null
+IF(@IdTarea > 0)
+BEGIN
+	UPDATE Tarea SET FechaCierreSolicitante = FechaCierreEncargado where IdTarea = @IdTarea
+	
+	SELECT @SecuencialHistorico = (ISNULL(MAX(Secuancial),0)+1)
+	FROM TareaEstado	
+	WHERE IdTarea = @IdTarea
+	group by IdTarea
+
+	INSERT INTO [dbo].[TareaEstado]
+           ([IdTarea]
+           ,[Secuancial]
+           ,[IdEstado]
+           ,[IdUsuario]
+           ,[FechaModificacion]
+           ,[Observacion])
+     VALUES
+		   (@IdTarea
+           ,@SecuencialHistorico
+           ,3
+           ,'admin'
+           ,getdate()
+           ,'CERRADO AUTOMATICO')
+
+	 SELECT @EsRecursivo = TareaConcurrente FROM Tarea where IdTarea = @IdTarea
+	 IF(@EsRecursivo = 1)
+		BEGIN
+			exec sp_crear_tarea_concurrente @IdTarea
+		END
+END
--- Tareas/Database/dbo/Tables/TareaArchivoAdjunto.sql
+++ Tareas/Database/dbo/Tables/TareaArchivoAdjunto.sql
@@ -1,11 +1,14 @@
 ﻿CREATE TABLE [dbo].[TareaArchivoAdjunto] (
     [IdTarea]       NUMERIC (18)   NOT NULL,
     [Secuencial]    INT            NOT NULL,
-    [NombreArchivo] VARCHAR (1000) NULL,
-    [Archivo]       IMAGE          NULL,
+    [NombreArchivo] VARCHAR (1000) NOT NULL,
+    [Archivo]       IMAGE          NOT NULL,
+    [TipoArchivo]   VARCHAR (MAX)  NOT NULL,
     CONSTRAINT [PK_TareaArchivoAdjunto] PRIMARY KEY CLUSTERED ([IdTarea] ASC, [Secuencial] ASC),
     CONSTRAINT [FK_TareaArchivoAdjunto_Tarea] FOREIGN KEY ([IdTarea]) REFERENCES [dbo].[Tarea] ([IdTarea])
 );
+
+
 
 
 
